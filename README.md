4105686bc390c6de1ce407f164e3e28c6bc57df7
Alex Geis <siegxela@gmail.com>
2022-08-25 11:53:52 -0600

    update readme for init commit and add domCreate()

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# helper-functions
+# Helper Functions
+
+This repo serves as an ongoing collection of algorithm solutions in Javascript.
+
+## Problems being helped
+
+- dom manipulation (creation/add class/add attr) - **(domCreate)**
+- flatten multidimensional array - **(concatAll)**
---
+++ js/domCreate.js
@@ -0,0 +1,25 @@
+/**
+ * @name domCreate
+ * @description takes input of HTML element type, array of classes to attach to DOM element, and object with key/value pairs of attributes to apply to the DOM element
+ * @param type string - HTML element (e.g. "div", "img")
+ * @param classes array - array of strings with class names. for no classes, submit empty array [""]
+ * @param attributes object - key/value pairs of attributes
+ * @return DOM element with provided classes/attributes
+ */
+function domCreate(type, classes, attributes) {
+	const element = document.createElement(type);
+
+	if (Array.isArray(classes) && classes.length > 0) {
+		for (let i = 0; i < classes.length; i++) {
+			const className = classes[i];
+			element.classList.add(className);
+		}
+	}
+
+	for (const attribute in attributes) {
+		element.setAttribute(attribute, attributes[attribute]);
+	}
+	return element;
+}
+
+export { domCreate };
