518d01709e8f02ede3b7d488a623217b80a7ce8f
Alex Geis <siegxela@gmail.com>
2022-09-13 11:02:58 -0600

    update memoize f(n) description and add 2 boilerplate examples

--- js/memoization.js
+++ js/memoization.js
@@ -1,10 +1,37 @@
 /**
- * @name removeClass
- * @description removes a CSS class from a DOM element
- * @param el DOM node - HTML element (e.g. "div", "img")
- * @param className string - name of class
- * @return no value - side effect is removed class from element
+ * @name memoize
+ * @description memoization involves storin results of expensive function calls
+ * @param value input to which logic will be applied
+ * @return value of computation (simultaneously storing this result to a cache object)
  */
+
+// EXAMPLE 1
+function memoize(value, memo = {}) {
+	if (value in memo) return memo[n];
+
+	// function logic
+	memo[n] = (function () {})();
+
+	return memo[n];
+}
+
+// EXAMPLE 2
+function memoize2(value) {
+	// if cache object is not created yet, we instantiate the object
+	if (!memoize2.cache) {
+		memoize2.cache = {};
+	}
+	// if our computed answer already exists in the cache object, return this saved value
+	if (memoize2.cache[value] !== undefined) {
+		return memoize2.cache[value];
+	}
+
+	// function logic
+	let resultFromLogic = (function () {})();
+
+	// store the result in the cache as we return it
+	return (memoize2.cache[value] = resultFromLogic);
+}
 
 // EXAMPLE FOR CALCULATING PRIME NUMBERS
 function isPrime(value) {
