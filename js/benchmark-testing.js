c983b8dca1708d9b34d30f5a2cbed3eb9dadec1b
Alex Geis <siegxela@gmail.com>
2022-09-21 15:45:15 -0600

    add benchmarking testing (dev env) solution

--- js/benchmark-testing.js
+++ js/benchmark-testing.js
@@ -1,7 +1,17 @@
 // below benchmarking function is for *development* environments only, not production
 
+// using console.time and console.timeEnd
+
 console.time("testing benchmark");
 
 // amazing function or process that we're testing
 
 console.timeEnd("testing benchmark");
+
+// using performance.now()
+
+const start = performance.now();
+// Javascript logic...
+const end = performance.now();
+
+console.log("This took " + (end - start) + "ms to complete");
