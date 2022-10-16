de10e0714efd49fbc6220fea71afc3e047d1107a
Alex Geis <siegxela@gmail.com>
2022-10-16 17:44:09 -0600

    add new UUID solution

--- js/uuid.js
+++ js/uuid.js
@@ -6,3 +6,15 @@ module.exports = () =>
 		.substring(1);
 
 // output example: e0a4
+
+// solution 2
+
+const UUIDGeneratorBrowser = () =>
+	([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+		(
+			c ^
+			(crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+		).toString(16)
+	);
+
+UUIDGeneratorBrowser(); // '7982fcfe-5721-4632-bede-
